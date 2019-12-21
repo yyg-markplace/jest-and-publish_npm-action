3fd711d0cbf4237d6f7904d956626aa03459b0ef
yanyong <454812511@qq.com>
2019-12-21 15:10:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const childProcess = require('child_process')
 
 
 let p_install_jest = childProcess.exec("sudo npm install -g jest" )
-
+p_install_jest.on('exit', (code) => {
+    console.log(`退出码: ${code}`);
+})
 
   
 let run = async function(){
@@ -27,27 +29,26 @@ let run = async function(){
 
     console.log("准备jest测试")
     //await exec.exec('jest', ['--json', '--outputFile' , 'jest-result.json']);
-    p_install_jest.on('exit', (code) => {
-        console.log(`退出码: ${code}`);
+    
 
-        await exec.exec('jest', [ '--json','--outputFile' , 'jest-result.json']);
-        console.log("jest测试结束 , 并将结果输出在jest-result.json文件中")
+    await exec.exec('jest', [ '--json','--outputFile' , 'jest-result.json']);
+    console.log("jest测试结束 , 并将结果输出在jest-result.json文件中")
+    
+    console.log("读取 json 文件,查看 jest 测试结果")
+    fs.readFile('jest-result.json',function(err,data){
+        if(err){
+            return console.error(err);
+        }
+        //先将 data 转换成字符串 , 然后用 json反序列化
+        let jest_result=JSON.parse( data.toString())
         
-        console.log("读取 json 文件,查看 jest 测试结果")
-        fs.readFile('jest-result.json',function(err,data){
-            if(err){
-                return console.error(err);
-            }
-            //先将 data 转换成字符串 , 然后用 json反序列化
-            let jest_result=JSON.parse( data.toString())
-            
-            if(jest_result.success){
-                console.log("jest 测试成功")
-            } else{
-                console.log("jest 测试失败")
-            }
-        })                      
-      });
+        if(jest_result.success){
+            console.log("jest 测试成功")
+        } else{
+            console.log("jest 测试失败")
+        }
+    })                      
+    
     console.log("jest测试结束 , 并将结果输出在jest-result.json文件中")
   }
   
