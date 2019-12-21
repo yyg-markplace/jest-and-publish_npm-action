840e33370b9be4f00e264f5f815d531e7faaafc6
yanyong <454812511@qq.com>
2019-12-21 13:33:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const { GitHub, context } = require("@actions/github");
 const exec = require('@actions/exec');
 const fs = require("fs")
 const core = require('@actions/core');
+const childProcess = require('child_process')
 
 
+
+let p_install_jest = childProcess.exec("sudo npm install -g jest" )
+
+p_install_jest.on('exit', (code) => {
+    console.log(`退出码: ${code}`);
+  });
+  
 let run = async function(){
     
+
+
     console.log("准备 git 检出")
     await exec.exec('git', ['clone', context.payload.repository.git_url]);
     console.log("git 检出 完毕")
@@ -19,7 +29,7 @@ let run = async function(){
 
     console.log("准备jest测试")
     //await exec.exec('jest', ['--json', '--outputFile' , 'jest-result.json']);
-    
+
     await exec.exec('jest', [ '--json','--outputFile' , 'jest-result.json']);
     console.log("jest测试结束 , 并将结果输出在jest-result.json文件中")
 
@@ -41,4 +51,4 @@ let run = async function(){
     console.log("jest测试结束 , 并将结果输出在jest-result.json文件中")
   }
   
-  run ()
+  //run ()
