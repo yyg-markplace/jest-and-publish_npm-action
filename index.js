41f2b4292b704980200277e91bae8e8d31c79566
yanyong <454812511@qq.com>
2019-12-21 13:07:21 +0800

    00

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ let run = async function(){
     console.log("jest安装完毕")
 
     console.log("准备jest测试")
-    await exec.exec('jest', ['--json', '--outputFile' , 'jest-result.json']);
+    //await exec.exec('jest', ['--json', '--outputFile' , 'jest-result.json']);
+    await exec.exec('jest', [ '--outputFile' , 'jest-result.json']);
     console.log("jest测试结束 , 并将结果输出在jest-result.json文件中")
 
     
