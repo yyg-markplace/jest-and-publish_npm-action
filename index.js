d6e791f7ca0a2bd980f68ce19131fef7e3160d4b
yanyong <454812511@qq.com>
2019-12-21 20:15:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const childProcess = require('child_process')
 let result_p_install_jest = 1
 let result_p_git_clone = 1
 let result_p_generate_package = 1
-let result_p_run_jest = 1
+//初始化 超时标志位 0 ,  当多进程都返回 0 的时候 , 进入循环中 , 然后在循环中间超时标志设为 1 ,防止多次运行
+let timeout_status = 0
 
 //使用多线程去全局安装 jest
 let p_install_jest = childProcess.exec("sudo npm install -g jest" )
@@ -24,8 +25,28 @@ let p_generate_package = childProcess.exec(package_contain)
 //运行 jest 命令
 //let p_run_jest = childProcess.exec( 'jest --json --outputFile jest-result.json')
 
+//还有一个创建 npmrc 文件使用 多进程生成文件
 
+let run_jest_output_result = async function(){
 
+    await exec.exec('jest', [ '--json','--outputFile' , 'jest-result.json']);
+    console.log("jest测试结束 , 并将结果输出在jest-result.json文件中")
+    
+    console.log("读取 json 文件,查看 jest 测试结果")
+    fs.readFile('jest-result.json',function(err,data){
+        if(err){
+            return console.error(err);
+        }
+        //先将 data 转换成字符串 , 然后用 json反序列化
+        let jest_result=JSON.parse( data.toString())
+        
+        if(jest_result.success){
+            console.log("jest 测试成功")
+        } else{
+            console.log("jest 测试失败")
+        }
+    }) 
+}
 //回调函数,  正常退出返回代码 0 
 p_install_jest.on('exit', (code) => {
     result_p_install_jest = code
@@ -38,31 +59,18 @@ p_generate_package.on('exit', (code) => {
 })
 
 
+
 let time = setInterval(function(){
-    if(!(result_p_install_jest + result_p_git_clone + result_p_generate_package )){
+    if(!(result_p_install_jest + result_p_git_clone + result_p_generate_package + timeout_status)){
+        // 进入循环之后, 立即将超时标志设为 1 , 是if 中的表达式为 0 ,就不会再次执行这段逻辑了
+        timeout_status =1
+
         console.log("安装 jest , 克隆宿主仓库 , 新建 package.json 完成")
-        let p_run_jest = childProcess.exec( 'jest --json --outputFile jest-result.json')
-        
-        p_run_jest.on('exit', (code) => {
-            //result_p_run_jest = code
-            if(!code){
-                console.log("读取 json 文件,查看 jest 测试结果")
-                fs.readFile('jest-result.json',function(err,data){
-                    if(err){
-                        return console.error(err);
-                    }
-                    //先将 data 转换成字符串 , 然后用 json反序列化
-                    let jest_result=JSON.parse( data.toString())
-                    
-                    if(jest_result.success){
-                        console.log("jest 测试成功")
-                    } else{
-                        console.log("jest 测试失败")
-                    }
-                })  
-            }
-            
-        })
+        //卫生这里还是使用 多进程 , 不是使用异步
+        //还是使用异步的好
+        run_jest_output_result ()
+
+        //这一个异步执行完毕之后, 就删除这个循环定时器
         clearInterval(time)
     }
 } , 300)
